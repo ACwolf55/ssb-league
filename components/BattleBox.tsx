5df07e66970ddf751aa0f10fb73ea1a7fa1b41a9
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2025-08-10 22:31:59 -0400

    jsx return type out

--- components/BattleBox.tsx
+++ components/BattleBox.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-export default function BattleBox({ name }: { name: string }): JSX.Element {
+export default function BattleBox({ name }: { name: string }) {
 
   return (
     <div className="bg-white text-black rounded-md h-30 w-30 px p-2 shadow text-center text-sm border border-gray-300">
